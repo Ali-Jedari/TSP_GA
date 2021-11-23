092bbe2e7e9dc1c4ae28e2eb5e1be273525363d7
Ali Jedari Heidarzadeh <77022845+JedariAli@users.noreply.github.com>
2021-11-24 00:50:07 +0330

    Update TSP.py

--- Genetic_Algorithm/TSP/TSP.py
+++ Genetic_Algorithm/TSP/TSP.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+'''
+Travelling Salesman Problem with 51 cities
+using Genetic Algorithm
+'''
 
 from random import random, randrange, shuffle
 from math import sqrt
@@ -14,20 +18,36 @@ for i in range(len(lines)):
     lines[i] = lines[i].split(' ')
     coordinations.append([int(lines[i][0]), int(lines[i][1]), int(lines[i][2])])
 
-
 def distance(city1, city2):
     return sqrt((coordinations[city1-1][1] - coordinations[city2-1][1]) ** 2 + 
                 (coordinations[city1-1][2] - coordinations[city2-1][2]) ** 2)
 
-
-def path(chromosome):
-    dist = 0
+def path_length(chromosome):
+    #it is assumed that a path always starts from city #1
+    d = 0
     for i in range(1, len(chromosome)):
-        dist += distance(chromosome[i-1], chromosome[i])
-    dist += distance(1, chromosome[0])
-    dist += distance(1, chromosome[-1])
-    return dist
+        d += distance(chromosome[i-1], chromosome[i])
+    d += distance(1, chromosome[0])
+    d += distance(1, chromosome[-1])
+    return d
 
+def crossover(parent1, parent2):
+    r = randrange(len(parent1))    
+    offspring = parent1[:r]
+    for i in range(len(parent2)):
+        if parent2[i] not in offspring:
+            offspring.append(parent2[i])
+    
+    return offspring
+
+def mutate(chromosome):
+    r1 = randrange(len(chromosome))
+    r2 = randrange(len(chromosome))
+    if r1 < r2:
+        chromosome[r1:r2] = chromosome[r1:r2][::-1]
+    else:
+        chromosome[r2:r1] = chromosome[r2:r1][::-1]
+    return chromosome
 
 def tsp():
     population_size = 40
@@ -40,15 +60,15 @@ def tsp():
     
     generation = 5000
     for gen in range(generation):
-        path_length = []
+        length = []
         for x in p:
-            path_length.append(path(x))
+            length.append(path_length(x))
         
-        Fitness = [1 / path_length[0]]
-        for i in range(1, len(path_length)):
-            Fitness.append(Fitness[i-1] + (1 / path_length[i]))
+        fitness = [1 / length[0]]
+        for i in range(1, len(length)):
+            fitness.append(fitness[i-1] + (1 / length[i]))
                 
-        best = p[path_length.index(min(path_length))]
+        best = p[length.index(min(length))]
         
         new_p = list()
         while len(new_p) < population_size - 1:
@@ -56,19 +76,19 @@ def tsp():
                 r1 = random()
                 r2 = random()
                 i = 0
-                for i in range(len(Fitness)):
+                for i in range(len(fitness)):
                     if i == 0:
-                        if 0 <= r1 <= Fitness[i]:
+                        if 0 <= r1 <= fitness[i]:
                             break
-                    elif Fitness[i-1] < r1 <= Fitness[i]:
+                    elif fitness[i-1] < r1 <= fitness[i]:
                         break
 
                 j = 0
-                for j in range(len(Fitness)):
+                for j in range(len(fitness)):
                     if j == 0:
-                        if 0 <= r2 <= Fitness[j]:
+                        if 0 <= r2 <= fitness[j]:
                             break
-                    elif Fitness[j-1] < r2 <= Fitness[j]:
+                    elif fitness[j-1] < r2 <= fitness[j]:
                         break
 
 
@@ -82,32 +102,15 @@ def tsp():
         new_p.append(best)
         
         p = new_p
-        print(f'generation: {gen+1} out of {generation}, shortest_path_length: {min(path_length)}', end = '\r')
+        print(f'generation: {gen+1} out of {generation}, shortest_path_length: {min(length)}', end = '\r')
 
     return p
 
+def main():
+    t1 = time()
+    p = tsp()
+    t2 = time()
+    print("\nTime elapsed: %.2f s" %(t2-t1))
 
-def crossover(parent1, parent2):
-    r = randrange(len(parent1))    
-    offspring = parent1[:r]
-    for i in range(len(parent2)):
-        if parent2[i] not in offspring:
-            offspring.append(parent2[i])
-    
-    return offspring
-
-
-def mutate(chromosome):
-    r1 = randrange(len(chromosome))
-    r2 = randrange(len(chromosome))
-    if r1 < r2:
-        chromosome[r1:r2] = chromosome[r1:r2][::-1]
-    else:
-        chromosome[r2:r1] = chromosome[r2:r1][::-1]
-    return chromosome
-
-
-t1 = time()
-p = tsp()
-t2 = time()
-print("\nTime elapsed: %.2f s" %(t2-t1))
+if __name__ == '__main__':
+    main()
