f446b0ec5526d08b973d612765bce7bc1d759f2d
Ali Jedari Heidarzadeh <77022845+JedariAli@users.noreply.github.com>
2021-11-24 00:44:22 +0330

    Add files via upload

---
+++ Genetic_Algorithm/TSP/TSP.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from random import random, randrange, shuffle
+from math import sqrt
+from time import time
+
+
+lines = open('TSP51.txt').readlines()
+coordinations = []
+for i in range(len(lines)):
+    lines[i] = lines[i].replace('\n', '')
+    lines[i] = lines[i].split(' ')
+    coordinations.append([int(lines[i][0]), int(lines[i][1]), int(lines[i][2])])
+
+
+def distance(city1, city2):
+    return sqrt((coordinations[city1-1][1] - coordinations[city2-1][1]) ** 2 + 
+                (coordinations[city1-1][2] - coordinations[city2-1][2]) ** 2)
+
+
+def path(chromosome):
+    dist = 0
+    for i in range(1, len(chromosome)):
+        dist += distance(chromosome[i-1], chromosome[i])
+    dist += distance(1, chromosome[0])
+    dist += distance(1, chromosome[-1])
+    return dist
+
+
+def tsp():
+    population_size = 40
+    p = list()
+    cities = [i for i in range(2, 52)]
+    for i in range(population_size):
+        temp_cities = cities.copy()
+        shuffle(temp_cities)
+        p.append(temp_cities)
+    
+    generation = 5000
+    for gen in range(generation):
+        path_length = []
+        for x in p:
+            path_length.append(path(x))
+        
+        Fitness = [1 / path_length[0]]
+        for i in range(1, len(path_length)):
+            Fitness.append(Fitness[i-1] + (1 / path_length[i]))
+                
+        best = p[path_length.index(min(path_length))]
+        
+        new_p = list()
+        while len(new_p) < population_size - 1:
+            if random() <= 0.90:
+                r1 = random()
+                r2 = random()
+                i = 0
+                for i in range(len(Fitness)):
+                    if i == 0:
+                        if 0 <= r1 <= Fitness[i]:
+                            break
+                    elif Fitness[i-1] < r1 <= Fitness[i]:
+                        break
+
+                j = 0
+                for j in range(len(Fitness)):
+                    if j == 0:
+                        if 0 <= r2 <= Fitness[j]:
+                            break
+                    elif Fitness[j-1] < r2 <= Fitness[j]:
+                        break
+
+
+                    offspring = crossover(p[i], p[j])
+                    new_p.append(offspring)
+
+        for indx in range(len(new_p)):
+            if random() <= 0.1:
+                new_p[indx] = mutate(new_p[indx])
+        
+        new_p.append(best)
+        
+        p = new_p
+        print(f'generation: {gen+1} out of {generation}, shortest_path_length: {min(path_length)}', end = '\r')
+
+    return p
+
+
+def crossover(parent1, parent2):
+    r = randrange(len(parent1))    
+    offspring = parent1[:r]
+    for i in range(len(parent2)):
+        if parent2[i] not in offspring:
+            offspring.append(parent2[i])
+    
+    return offspring
+
+
+def mutate(chromosome):
+    r1 = randrange(len(chromosome))
+    r2 = randrange(len(chromosome))
+    if r1 < r2:
+        chromosome[r1:r2] = chromosome[r1:r2][::-1]
+    else:
+        chromosome[r2:r1] = chromosome[r2:r1][::-1]
+    return chromosome
+
+
+t1 = time()
+p = tsp()
+t2 = time()
+print("\nTime elapsed: %.2f s" %(t2-t1))
